64730b557c29b3e133c2308f01fb7ea07fee8964
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-17 20:08:06 -0700

    made a few adj in index.js file

--- js/index.js
+++ js/index.js
@@ -13,6 +13,8 @@ function checkCookies(){
     document.getElementById("demo").innerHTML = text;
 }
 
+console.log(checkCookies)
+
 //Event listener
 
 const btn = document.getElementById("btn").addEventListener("click", displayDate);
@@ -20,3 +22,5 @@ const btn = document.getElementById("btn").addEventListener("click", displayDate
 function displayDate() {
     document.getElementById("demo").innerHTML = Date();
 }
+
+console.log(displayDate)
