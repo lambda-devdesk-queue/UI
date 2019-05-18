5dd4a278e75b14d3e90a566798c0f8f2e3726287
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-18 00:37:18 -0700

    added a few JS and DOM in landing page

--- js/index.js
+++ js/index.js
@@ -1,19 +1,4 @@
-//added an Event
-//onload and onunload events triggered when user enters or leaves the page
-//used to check visitor's browser type and version
 
-
-function checkCookies(){
-    var text = "";
-    if (navigator.cookieEnabled == true){
-        text = "Cookies are emabled.";
-    } else {
-        text = "Cookies are not enabled.";
-    }
-    document.getElementById("demo").innerHTML = text;
-}
-
-console.log(checkCookies)
 
 //Event listener
 
@@ -25,8 +10,3 @@ function displayDate() {
 
 console.log(displayDate)
 
-const d = new Date();
-d.setFullYear(2019);
-document.getElementById("copyright").innerHTML = d;
-
-console.log(d);
